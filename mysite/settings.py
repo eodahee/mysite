3fab9031f5c9ae823c892c8e25b21b9adf388b18
어다희 <eodahee@naver.com>
2018-10-25 11:05:20 +0900

    투표시스템_2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w8zd6xooxan8e=5bf&ds4a@2khovacdql&9t=j0jzzt=m3y+04'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
